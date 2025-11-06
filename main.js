d1e9b8089d671e77e9ee66b2cbc5d7824711ab4d
sonny4546 <jumong4546@gmail.com>
2025-11-06 21:29:41 +0800

    fix password js

--- main.js
+++ main.js
@@ -25,34 +25,32 @@ var mtn_shd2 = document.getElementById("mountain-shd2");
 var mtn_shd3 = document.getElementById("mountain-shd3");
 let originalValue = "none";
 
-if (passInput == true) {
-    passInput.addEventListener('input', function() {
-        originalValue = this.value; // Get the actual input value
-    });
+passInput.addEventListener('input', function() {
+    originalValue = this.value; // Get the actual input value
+});
 
-    let keypress = 0;
+let keypress = 0;
 
-    // Execute a function when the user presses a key on the keyboard
-    passInput.addEventListener("keypress", function(event) {
-        // If the user presses the "Enter" key on the keyboard
-        if (event.key === "Enter") {
-            // Cancel the default action, if needed
-            event.preventDefault();
-            if (originalValue === "11/12/07") {
-                body.style.animation = "fadeInColor 0.5s ease-in-out forwards";
-                maindiv.style.animation = "fadeOut 0.25s ease-in-out forwards";
-                window.location.replace("/valley.html");
-            } else {
-                keypress += 1;
-                if (keypress == 5) {
-                    hint.style.color = "greenyellow";
-                }
-                console.log(keypress);
-                console.log(originalValue);
+// Execute a function when the user presses a key on the keyboard
+passInput.addEventListener("keypress", function(event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        if (originalValue === "11/12/07") {
+            body.style.animation = "fadeInColor 0.5s ease-in-out forwards";
+            maindiv.style.animation = "fadeOut 0.25s ease-in-out forwards";
+            window.location.replace("/valley.html");
+        } else {
+            keypress += 1;
+            if (keypress == 5) {
+                hint.style.color = "greenyellow";
             }
+            console.log(keypress);
+            console.log(originalValue);
         }
-    });
-}
+    }
+});
 
 // Define the function to be executed on click
 function handleClick() {
